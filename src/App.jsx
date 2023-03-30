9be565dbef48960957b8ea8ff51acbc5dcd63ca5
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-03-30 21:47:17 +0600

    Install Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Project Create Successfully</h1>
+      <button className='btn btn-warning'>Install Bootstrap</button>
     </div>
   )
 }
